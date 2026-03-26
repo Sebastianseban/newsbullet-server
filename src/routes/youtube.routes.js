c943792341ce7fbca17eece80ff1967e1dc4af7f
sebastian <sebastianseban0105@gmail.com>
2026-03-26 17:25:47 +0530

    feat: rate limit youtube routes

--- src/routes/youtube.routes.js
+++ src/routes/youtube.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { getVideosFromDB } from "../controllers/youtube.controller.js";
 import { verifyJWT } from "../middleware/auth.Middleware.js";
-
+import { createRateLimiter } from "../middleware/rateLimit.middleware.js";
 
 const router = express.Router();
+const youtubeLimiter = createRateLimiter({
+  windowMs: 60 * 1000,
+  max: 60,
+  message: "Too many YouTube requests. Please try again later.",
+});
 
-
-router.get("/",verifyJWT, getVideosFromDB);
+router.get("/", verifyJWT, youtubeLimiter, getVideosFromDB);
 
 export default router;
