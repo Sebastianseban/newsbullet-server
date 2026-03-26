ce333a9242ab503e168943b9f84c35902818d09e
sebastian <sebastianseban0105@gmail.com>
2026-03-26 17:25:47 +0530

    feat: rate limit news routes

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -7,13 +7,19 @@ import {
   createNews,
 } from "../controllers/news.contoller.js";
 import { verifyJWT } from "../middleware/auth.Middleware.js";
+import { createRateLimiter } from "../middleware/rateLimit.middleware.js";
 
 const router = express.Router();
+const newsReadLimiter = createRateLimiter({
+  windowMs: 60 * 1000,
+  max: 120,
+  message: "Too many news requests. Please slow down.",
+});
 
 // CRUD routes
 router.post("/", verifyJWT, createNews);
-router.get("/", getAllNews);
-router.get("/:slug", getNewsBySlug);
+router.get("/", newsReadLimiter, getAllNews);
+router.get("/:slug", newsReadLimiter, getNewsBySlug);
 router.put("/:slug", verifyJWT, updateNews);
 router.delete("/:slug", verifyJWT, deleteNews);
 
