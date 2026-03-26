746e9532f18277d7166930a6001ec3187f3d7560
sebastian <sebastianseban0105@gmail.com>
2026-03-26 17:25:46 +0530

    chore: add youtube video indexes

--- src/models/YoutubeVideo.js
+++ src/models/YoutubeVideo.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const youtubeVideoSchema = new mongoose.Schema({
-  videoId: { type: String, required: true, unique: true },
-  title: String,
-  description: String,
-  publishedAt: { type: Date }, // <-- correct
-  thumbnail: String,
-  link: String,
-});
+const youtubeVideoSchema = new mongoose.Schema(
+  {
+    videoId: { type: String, required: true, unique: true, index: true },
+    title: String,
+    description: String,
+    publishedAt: { type: Date, index: true },
+    thumbnail: String,
+    link: String,
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("YoutubeVideo", youtubeVideoSchema);
